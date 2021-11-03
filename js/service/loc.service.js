28055d2ff2ded2e1953f038b0129c708b459a13f
Adi-Davidovich <adidaflower@gmail.com>
2021-11-04 00:22:53 +0200

    added func search location

--- js/service/loc.service.js
+++ js/service/loc.service.js
@@ -1,9 +1,11 @@
 export const locService = {
     getLocs,
     saveloc,
-    deleteLoc
+    deleteLoc,
+    searchLocation
 }
 import { storageService } from './storage.servic.js'
+import { mapService } from './map.service.js'
 
 var gIdx = 0
 
@@ -29,19 +31,48 @@ function creatNewLocation(lat, lng, id, title, weather = null, createdAt = Date.
         updatedAt
     }
     locs.push(loc)
+    mapService.addMarker({ lat, lng });
     storageService.saveToStorage('locs', locs)
     console.log(locs)
 }
 
+
 function saveloc(lat, lng) {
     var title = document.querySelector(".save-loc").value
     creatNewLocation(lat, lng, gIdx++, title)
+    infoWindow.close(gMap);
 }
+
 
 function deleteLoc(locId) {
     const idx = locs.findIndex(loc => locId === loc.id)
     locs.splice(idx, 1);
     console.log(locs)
     storageService.saveToStorage('locs', locs)
-   
+}
+
+
+function searchLocation(searchValue) {
+     _connectGoogleGeocodeApi(searchValue)
+     .then(res => {
+         console.log(res.lat);
+         const lat = res.lat
+         const lng = res.lng
+         const title = searchValue
+         creatNewLocation(lat, lng, gIdx++, title)
+         mapService.panTo(lat, lng);
+     })
+}
+
+
+function _connectGoogleGeocodeApi(address) {
+    const API_KEY = 'AIzaSyCc7Qnw5U_LAip2s7WPpW-1UBsMnil8DMA';
+    return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${API_KEY}`)
+        .then(res => {
+            console.log(res);
+            if(res.data.results.length > 0) {
+                const {lat, lng} = res.data.results[0].geometry.location
+                return {lat, lng}
+            }
+        })
 }
