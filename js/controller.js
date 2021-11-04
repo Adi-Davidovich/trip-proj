b3c10ea1890c3ee470f62e259b77121c567a8cfd
Adi-Davidovich <adidaflower@gmail.com>
2021-11-04 03:16:56 +0200

    added functions and fixed bugs

--- js/controller.js
+++ js/controller.js
@@ -3,18 +3,16 @@ import { mapService } from './service/map.service.js'
 
 
 window.onload = onInit;
-window.onAddMarker = onAddMarker;
-window.onPanTo = onPanTo;
-// window.onGetLocs = onGetLocs;
 window.onGetUserPos = onGetUserPos;
-window.onSaveLoc = onSaveLoc
-window.onDeleteLoc = onDeleteLoc
-window.onSearchLocation = onSearchLocation
+window.onSaveLoc = onSaveLoc;
+window.onDeleteLoc = onDeleteLoc;
+window.onGoTo = onGoTo;
+window.onPanTo = onPanTo;
+window.onSearchLocation = onSearchLocation;
 
 function onInit() {
     mapService.initMap()
         .then(() => {
-            console.log('Map is ready');
             _renderLoc()
         })
         .catch(() => console.log('Error: cannot init map'));
@@ -22,41 +20,32 @@ function onInit() {
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
-    console.log('Getting Pos');
     return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject)
+        navigator.geolocation.getCurrentPosition(resolve, reject);
     })
 }
 
-function onAddMarker() {
-    console.log('Adding a marker');
-    mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
-}
-
-// function onGetLocs() {
-//     locService.getLocs()
-//         .then(locs => {
-//             console.log('Locations:', locs)
-//             document.querySelector('.locs').innerText = JSON.stringify(locs)
-//         })
-// }
 
 function onGetUserPos() {
     getPosition()
         .then(pos => {
-            console.log('User position is:', pos.coords);
             document.querySelector('.user-pos').innerText =
-                `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
-                mapService.panTo(pos.coords.latitude, pos.coords.longitude);  
+                `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`;
+            mapService.panTo(pos.coords.latitude, pos.coords.longitude);
+            const myPos = { lat: pos.coords.latitude, lng: pos.coords.longitude };
+            mapService.addMarker(myPos, 'My location');
         })
         .catch(err => {
             console.log('err!!!', err);
         })
 }
+
+
 function onPanTo(lat, lng) {
     console.log('Panning the Map');
     mapService.panTo(lat, lng);
 }
+
 
 function onSaveLoc(lat, lng) {
     locService.saveloc(lat, lng)
@@ -64,26 +53,38 @@ function onSaveLoc(lat, lng) {
 }
 
 function _renderLoc() {
+    const elLocs = document.querySelector(".locs");
     locService.getLocs().then(locs => {
-        if (!locs || !locs.length) return
-    var HtmlTable = `<table><thead><th>Title</th><th>Location</th><th></th><th></th></thead><tbody>`
-    var Htmls = locs.map(loc => {
-        return `<tr>
-            <td>${loc.title}</td>
-            <td>${loc.lat, loc.lng}</td>
-            <td><button onClick="onPanTo(${loc.lat}, ${loc.lng})">go</button></td>
-            <td><button onclick="onDeleteLoc((${loc.id}))">delete</button></td>
-            </tr>`
-    })
-    var HtmlTable1 = `</tbody></table>`
-    document.querySelector(".locs").innerHTML = HtmlTable + Htmls.join('') + HtmlTable1;
+        if (!locs || !locs.length) elLocs.innerHTML = 'No saved locations <div>To save a location click on the desired location on the map</div>'
+        else {
+            var HtmlTable = `<table><thead><th>Title</th><th>Location</th><th></th><th></th></thead><tbody>`
+            var Htmls = locs.map(loc => {
+                return `<tr>
+                            <td>${loc.title}</td>
+                            <td>${loc.lat, loc.lng}</td>
+                            <td><button onClick="onGoTo(${loc.lat}, ${loc.lng}, '${loc.title}')">go</button></td>
+                            <td><button onclick="onDeleteLoc('${loc.id}', '${loc.title}')">delete</button></td>
+                        </tr>`
+            })
+            var HtmlTable1 = `</tbody></table>`
+            elLocs.innerHTML = HtmlTable + Htmls.join('') + HtmlTable1;
+        }
     });
 }
 
 
-function onDeleteLoc(locId) {
+function onDeleteLoc(locId, locTitle) {
     locService.deleteLoc(locId)
+    mapService.deleteMarker(locTitle)
     _renderLoc()
+    // const marker = mapService.addMarker(pos, title);
+    // marker.setMap(null);
+}
+
+function onGoTo(lat, lng, title) {
+    mapService.panTo(lat, lng);
+    const pos = { lat: lat, lng: lng };
+    mapService.addMarker(pos, title);
 }
 
 function onSearchLocation() {
